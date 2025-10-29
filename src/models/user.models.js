2932c4714a59b283f8fb44cede5e452b58e91790
pranab <pranabm406@gmail.com>
2025-10-29 21:57:32 +0530

    user-schema

---
+++ src/models/user.models.js
@@ -0,0 +1,54 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    avater: {
+      type: {
+        url: String,
+        localpath: String,
+      },
+      default: "https://placehold.co/200x200",
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    refreshTokens: {
+      type: String,
+    },
+    forgotPasswordToken: {
+      type: String,
+    },
+    forgotPasswordExpiry: {
+      type: Date,
+    },
+    emailVerificationToken: {
+      type: String,
+    },
+    emailVerificationExpiry: {
+      type: Date,
+    },
+  },
+  { timestamps: true },
+);
+
+export const User = model("User", userSchema);
