c4d70479169a520ca4d017a75d81daad60dfa523
pranab <pranabm406@gmail.com>
2025-10-29 22:13:28 +0530

    pass-hash pre-hook

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -50,5 +51,14 @@ const userSchema = new Schema(
   },
   { timestamps: true },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  // const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
 
 export const User = model("User", userSchema);
